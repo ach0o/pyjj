f71578db0809336e761b30f4e33c0244151bb916
achooan <88soldieron@gmail.com>
2019-06-06 22:23:43 +0900

    Edit comments for each pyjj commands
    - update docstring
    - update help arguments

--- pyjj/__init__.py
+++ pyjj/__init__.py
@@ -11,31 +11,38 @@ def msg(division, status, message) -> str:
 pass_config = click.make_pass_decorator(PyjjConfig, ensure=True)
 
 
-@click.group()
+@click.group(help="A CLI tool for bookmark management")
 @pass_config
 def pyjj(config):
-    """A CLI tool for bookmark management."""
+    """A CLI tool for bookmark management
+
+    :param object config: an object with the current context
+    """
     config.parse()
     config.db = Db(division=config.division)
     config.db.setup()
 
 
-@pyjj.command()
+@pyjj.command(help="Switch to a different table")
 @click.argument("division")
 @pass_config
 def use(config, division=str):
     """Switch to a different table
 
+    :param object config: an object with the current context
     :param str division: a name of the division
     """
     config.update(division=division)
     click.echo(f"Switched to {division}")
 
 
-@pyjj.command()
+@pyjj.command(help="Show a list of bookmarks")
 @pass_config
 def list(config):
-    """Show a list of bookmarks"""
+    """Show a list of bookmarks
+
+    :param object config: an object with the current context
+    """
     status, urls = config.db.list_urls()
     if not status:
         click.echo(msg(config.division, status, urls))
@@ -46,30 +53,43 @@ def list(config):
             click.echo(f"{id:^7} {url:70} {date}")
 
 
-@pyjj.command()
+@pyjj.command(help="Add a new bookmark")
 @click.argument("url")
 @pass_config
-def add(config, url):
-    """Add a new bookmark"""
+def add(config, url: str):
+    """Add a new bookmark
+
+    :param object config: an object with the current context
+    :param str url: an url to add to the database
+    """
     result = config.db.add_url(url)
     click.echo(msg(config.division, *result))
 
 
-@pyjj.command()
+@pyjj.command(help="Edit a bookmark")
 @click.argument("id")
 @click.option("--url", help="Edit url")
 @pass_config
-def edit(config, url, id):
-    """Edit a bookmark"""
+def edit(config, id: int, url: str):
+    """Edit a bookmark
+
+    :param object config: an object with the current context
+    :param int id: an id of url to edit
+    :param str url: an url to add to the database
+    """
     result = config.db.edit_url(id, url)
     click.echo(msg(config.division, *result))
 
 
+@pyjj.command(help="Remove a bookmark")
 @click.argument("id")
-@pyjj.command()
 @pass_config
 def remove(config, id):
-    """remove a bookmark"""
+    """Remove a bookmark
+
+    :param object config: an object with the current context
+    :param int id: an id of url to delete
+    """
     result = config.db.remove_url(id)
     click.echo(msg(config.division, *result))
 
