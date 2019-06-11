222167d836910de955148ea933350b1320432df3
achooan <88soldieron@gmail.com>
2019-06-11 21:52:22 +0900

    Update message colors for better visibility

--- pyjj/messages.py
+++ pyjj/messages.py
@@ -1,8 +1,18 @@
 class Colors:
-    RED = "\u001b[91m"
-    GREEN = "\u001b[92m"
+    """ANSI Escape Code on colors for colorful stdout
+    """
+
+    RED = "\u001b[38;5;197m"
+    GREEN = "\u001b[38;5;157m"
 
 
-def msg(division, status, message) -> str:
+def msg(division: str, status: bool, message: str) -> str:
+    """Return a colorize string depends on the status
+
+    :param str division: a current division
+    :param bool status: a status of the message
+    :param str message: a message to display on the console
+    :return: string
+    """
     color = Colors.GREEN if status else Colors.RED
     return f"{color}[{division:^10}] {message}"
