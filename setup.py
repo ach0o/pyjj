6c78639d4d8964da088c99e6aacab8934a35ca64
achooan <88soldieron@gmail.com>
2019-06-17 21:17:37 +0900

    Update package information
    - add license, readme, python version

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
+from os import path
+
 from setuptools import setup
+
+
+def get_long_description() -> str:
+    readme = path.join(path.dirname(path.abspath(__file__)), "README.md")
+    with open(readme, encoding="utf8") as f:
+        return f.read()
 
 
 setup(
     name="pyjj",
     version="1.0",
     author="Changhyun An",
-    author_email="88soldieron" "@" "gmail.com",
+    author_email="88soldieron@gmail.com",
     py_modules=["pyjj"],
     install_requires=["click"],
     maintainer="Changhyun An",
-    maintainer_email="88soldieron" "@" "gmail.com",
+    maintainer_email="88soldieron@gmail.com",
     url="https://www.github.com/achooan/pyjj",
-    description="A command line interface for bookmark management",
+    license="MIT",
+    description="A bookmark management CLI",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     entry_points={"console_scripts": ["pyjj=pyjj:pyjj"]},
+    python_requires=">=3.6",
 )
