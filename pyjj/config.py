8d66fcc39f013a9edd4973d47f385c936b8bd6dd
achooan <88soldieron@gmail.com>
2019-07-06 11:18:40 +0900

    Add comments on pyjj config
    - Add descriptions on each methods

--- pyjj/config.py
+++ pyjj/config.py
@@ -4,6 +4,9 @@ from yaml import dump, Dumper, load, Loader
 
 
 def handle_exception(func):
+    """Handles exceptions raise from the input function
+    """
+
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
@@ -16,16 +19,23 @@ def handle_exception(func):
 
 
 class PyjjConfig:
+    """Class that parses and updates configurations from a configuration file: config.yaml
+    Possible configurations include,
+      - `division`: A space for bookmarks; equivalent to database name.
+    """
+
     def __init__(self):
         self.path = os.path.join(
             os.path.dirname(os.path.abspath(__file__)), "config.yaml"
         )
-        if not os.path.exists(self.path):
+        if not os.path.exists(self.path):  # create a config.yaml file if doesn't exist
             with open(self.path, "w") as file:
                 file.write("division: default")
 
     @handle_exception
     def parse(self) -> None:
+        """Parse config.yaml and set configurations to class attributes
+        """
         with open(self.path, "r") as file:
             try:
                 yml_content = load(file, Loader=Loader)
@@ -36,6 +46,8 @@ class PyjjConfig:
 
     @handle_exception
     def update(self, **kwargs) -> None:
+        """Overwrite config.yaml with class attributes
+        """
         try:
             dump(data=kwargs, stream=open(self.path, "w"), Dumper=Dumper)
             for key, val in kwargs.items():
